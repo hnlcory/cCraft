41c2caf59278a090a460bb9b2badac8cf64cea19
Cory Parker <school.coryparker@gmail.com>
2021-02-12 22:25:56 -1000

    Add files via upload
    test "world" basic movemet

--- TestGame/game.py
+++ TestGame/game.py
@@ -1,6 +1,5 @@
 from ursina import *
-from ursina.prefabs.first_person_controller import FirstPersonController
-
+from ursina.prefabs.first_person_controller import FirstPersonController 
 
 class Voxel(Button):
     def __init__(self, position= (0,0,0)):
@@ -10,14 +9,21 @@ class Voxel(Button):
             model= 'cube',
             origin_y=0.5,
             texture='white_cube',
-            color= color.white,
-            highlight_color = color.lime
-        )
+            color= color.color(0,0,random.uniform(0.9,1)),
+            highlight_color = color.gray)
+
+    def input(self,key):
+        if self.hovered:
+            if key == 'right mouse down':
+                voxel= Voxel(position = self.position + mouse.normal)
+            if key == 'left mouse down':
+                destroy(self)
 
 app = Ursina()
+window.title = 'cCraft Demo'
 
-for z in range(8):
-    for x in range(8):
+for z in range(20):
+    for x in range(20):
         voxel = Voxel(position = (x,0,z))
 player = FirstPersonController
 player()
